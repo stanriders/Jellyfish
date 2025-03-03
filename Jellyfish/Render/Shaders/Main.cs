a94e573ce4a283d2fccd8d5fa622e0e9b61fad36
StanR <hi@stanr.info>
2025-03-03 18:59:02 +0500

    PBR shader changes

--- Jellyfish/Render/Shaders/Main.cs
+++ Jellyfish/Render/Shaders/Main.cs
@@ -9,39 +9,41 @@ namespace Jellyfish.Render.Shaders;
 
 public class Main : Shader
 {
-    private readonly bool _usePhong;
-    private readonly int _phongExponent;
     private readonly Texture _diffuse;
     private readonly Texture? _normal;
+    private readonly Texture? _metRought;
 
-    public Main(string diffusePath, string? normalPath = null, bool usePhong = false, int phongExponent = 16) : 
+    public Main(string diffusePath, string? normalPath = null, string? metroughPath = null) : 
         base("shaders/Main.vert", null, "shaders/Main.frag")
     {
-        _usePhong = usePhong;
-        _phongExponent = phongExponent;
-        _diffuse = new Texture(diffusePath);
+        _diffuse = TextureManager.GetTexture(diffusePath, TextureTarget.Texture2d).Texture;
 
         if (!string.IsNullOrEmpty(normalPath))
         {
-            _normal = new Texture(normalPath);
+            _normal = TextureManager.GetTexture(normalPath, TextureTarget.Texture2d).Texture;
+        }
+
+        if (!string.IsNullOrEmpty(metroughPath))
+        {
+            _metRought = TextureManager.GetTexture(metroughPath, TextureTarget.Texture2d).Texture;
         }
     }
 
     public override void Bind()
     {
-        var camera = Camera.Instance;
-        if (camera == null)
+        var player = Player.Instance;
+        if (player == null)
             return;
 
         base.Bind();
 
-        SetVector3("cameraPos", camera.GetPropertyValue<Vector3>("Position"));
-        SetMatrix4("view", camera.GetViewMatrix());
-        SetMatrix4("projection", camera.GetProjectionMatrix());
+        SetVector3("cameraPos", player.GetPropertyValue<Vector3>("Position"));
+        SetMatrix4("view", player.GetViewMatrix());
+        SetMatrix4("projection", player.GetProjectionMatrix());
 
         var lights = LightManager.Lights.Where(x=> x.Source.Enabled).ToArray();
         SetInt("lightSourcesCount", lights.Length);
-        for (var i = 0; i < lights.Length; i++)
+        for (uint i = 0; i < lights.Length; i++)
         {
             var light = lights[i].Source;
             SetVector3($"lightSources[{i}].position", light.Position);
@@ -49,9 +51,9 @@ public class Main : Shader
             var rotationVector = Vector3.Transform(-Vector3.UnitY, light.Rotation);
             SetVector3($"lightSources[{i}].direction", rotationVector);
 
-            SetVector3($"lightSources[{i}].diffuse", new Vector3(light.Color.R, light.Color.G, light.Color.B));
-            SetVector3($"lightSources[{i}].ambient", new Vector3(light.Ambient.R, light.Ambient.G, light.Ambient.B));
-            SetFloat($"lightSources[{i}].brightness", light.Color.A);
+            SetVector3($"lightSources[{i}].diffuse", new Vector3(light.Color.X, light.Color.Y, light.Color.Z));
+            SetVector3($"lightSources[{i}].ambient", new Vector3(light.Ambient.X, light.Ambient.Y, light.Ambient.Z));
+            SetFloat($"lightSources[{i}].brightness", light.Color.W);
 
             var lightType = 0; // point
             if (light is Sun)
@@ -81,36 +83,49 @@ public class Main : Shader
 
             if (light.UseShadows)
             {
-                GL.ActiveTexture(TextureUnit.Texture2 + i);
+                GL.ActiveTexture(TextureUnit.Texture3 + i);
                 lights[i].ShadowRt!.Bind();
             }
+
+            SetBool($"lightSources[{i}].hasShadows", light.UseShadows);
         }
 
-        SetInt("usePhong", _usePhong ? 1 : 0);
-        SetInt("phongExponent", _phongExponent);
-
-        SetInt("useNormals", _normal != null ? 1 : 0);
+        SetBool("useNormals", _normal != null);
+        SetBool("usePbr", _metRought != null);
 
         _diffuse.Bind(0);
         _normal?.Bind(1);
+        _metRought?.Bind(2);
     }
 
     public override void Unbind()
     {
         GL.ActiveTexture(TextureUnit.Texture0);
-        GL.BindTexture(TextureTarget.Texture2D, 0);
+        GL.BindTexture(TextureTarget.Texture2d, 0);
 
         GL.ActiveTexture(TextureUnit.Texture1);
-        GL.BindTexture(TextureTarget.Texture2D, 0);
+        GL.BindTexture(TextureTarget.Texture2d, 0);
+
+        GL.ActiveTexture(TextureUnit.Texture2);
+        GL.BindTexture(TextureTarget.Texture2d, 0);
 
         var lights = LightManager.Lights.Where(x => x.Source.Enabled).ToArray();
         SetInt("lightSourcesCount", lights.Length);
-        for (var i = 0; i < lights.Length; i++)
+        for (uint i = 0; i < lights.Length; i++)
         {
-            GL.ActiveTexture(TextureUnit.Texture2 + i);
-            GL.BindTexture(TextureTarget.Texture2D, 0);
+            GL.ActiveTexture(TextureUnit.Texture3 + i);
+            GL.BindTexture(TextureTarget.Texture2d, 0);
         }
 
         base.Unbind();
+    }
+
+    public override void Unload()
+    {
+        _diffuse.Unload();
+        _normal?.Unload();
+        _metRought?.Unload();
+
+        base.Unload();
     }
 }
