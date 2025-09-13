29890ddcb925681efa977c17af94c353e64b4150
StanR <hi@stanr.info>
2025-09-14 03:58:24 +0500

    Improve RenderBuffer

--- Jellyfish/Render/Buffers/RenderBuffer.cs
+++ Jellyfish/Render/Buffers/RenderBuffer.cs
@@ -2,9 +2,12 @@
 
 namespace Jellyfish.Render.Buffers;
 
-public static class RenderBuffer
+public class RenderBuffer
 {
-    public static void Create(InternalFormat type, FramebufferAttachment attachment, int width, int heigth)
+    public readonly InternalFormat Type;
+    public readonly int Handle;
+
+    public static int Create(InternalFormat type, FramebufferAttachment attachment, int width, int heigth)
     {
         var renderBuffer = GL.GenRenderbuffer();
         GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, renderBuffer);
@@ -12,5 +15,23 @@ public static class RenderBuffer
         GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, 0);
 
         GL.FramebufferRenderbuffer(FramebufferTarget.Framebuffer, attachment, RenderbufferTarget.Renderbuffer, renderBuffer);
+
+        return renderBuffer;
+    }
+
+    public RenderBuffer(InternalFormat type, FramebufferAttachment attachment, int width, int heigth)
+    {
+        Type = type;
+        Handle = Create(type, attachment, width, heigth);
+    }
+
+    public void Bind()
+    {
+        GL.BindRenderbuffer(RenderbufferTarget.Renderbuffer, Handle);
+    }
+
+    public void UpdateSize(int width, int heigth)
+    {
+        GL.RenderbufferStorage(RenderbufferTarget.Renderbuffer, Type, width, heigth);
     }
 }
