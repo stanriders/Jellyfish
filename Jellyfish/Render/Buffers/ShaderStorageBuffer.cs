8196fa0b4115ce2fa9c0a00fd4507ff6761f214b
StanR <hi@stanr.info>
2025-09-22 17:31:16 +0500

    Implement SSBOs

---
+++ Jellyfish/Render/Buffers/ShaderStorageBuffer.cs
@@ -0,0 +1,51 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Runtime.InteropServices;
+using Jellyfish.Render.Shaders.Structs;
+
+namespace Jellyfish.Render.Buffers;
+
+public class ShaderStorageBuffer<T> where T: struct, IGpuStruct
+{
+    public readonly int Handle;
+
+    public ShaderStorageBuffer(string name, T data)
+    {
+        GL.CreateBuffer(out Handle);
+        GL.ObjectLabel(ObjectIdentifier.Buffer, (uint)Handle, name.Length, name);
+
+        var bufferSize = Marshal.SizeOf<T>();
+        GL.NamedBufferStorage(Handle, bufferSize, IntPtr.Zero, BufferStorageMask.DynamicStorageBit);
+
+        var ptr = Marshal.AllocHGlobal(bufferSize);
+        try
+        {
+            Marshal.StructureToPtr(data, ptr, false);
+            GL.NamedBufferSubData(Handle, IntPtr.Zero, bufferSize, ptr);
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+
+    public void Bind(uint binding)
+    {
+        GL.BindBufferBase(BufferTarget.ShaderStorageBuffer, binding, Handle);
+    }
+
+    public void UpdateData(T data)
+    {
+        var bufferSize = Marshal.SizeOf<T>();
+        var ptr = Marshal.AllocHGlobal(bufferSize);
+        try
+        {
+            Marshal.StructureToPtr(data, ptr, false);
+            GL.NamedBufferSubData(Handle, IntPtr.Zero, bufferSize, ptr);
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+}
---
+++ Jellyfish/Render/Shaders/Structs/IGpuStruct.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jellyfish.Render.Shaders.Structs;
+
+public interface IGpuStruct;
