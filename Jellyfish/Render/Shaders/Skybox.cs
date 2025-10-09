9ceb827f17306ebf66dcdd8a9349af341d61fb2a
StanR <hi@stanr.info>
2025-10-09 18:26:09 +0500

    Use view position in skybox instead of the viewport position

--- Jellyfish/Render/Shaders/Skybox.cs
+++ Jellyfish/Render/Shaders/Skybox.cs
@@ -3,20 +3,20 @@ using OpenTK.Mathematics;
 
 namespace Jellyfish.Render.Shaders;
 
-public class Skybox : Shader
+public class Skybox() : Shader("shaders/Skybox.vert", null, "shaders/Skybox.frag")
 {
-    public Skybox() : base("shaders/Skybox.vert", null, "shaders/Skybox.frag") { }
-
     public override void Bind()
     {
         base.Bind();
 
         var rotationVector = Vector3.Transform(Vector3.UnitY, Engine.LightManager.Sun!.Source.Rotation);
         SetVector3("uSunPos", rotationVector);
-        SetFloat("uViewHeight", Math.Max(0f, Engine.MainViewport.Position.Y));
+
+        var view = Engine.MainViewport.GetViewMatrix();
+        SetFloat("uViewHeight", Math.Max(0f, view.Inverted().ExtractTranslation().Y));
         SetFloat("uSunIntensity", Engine.LightManager.Sun.Source.Brightness * 4f);
 
-        SetMatrix4("view", Engine.MainViewport.GetViewMatrix().ClearTranslation());
+        SetMatrix4("view", view.ClearTranslation());
         SetMatrix4("projection", Engine.MainViewport.GetProjectionMatrix());
     }
 }
