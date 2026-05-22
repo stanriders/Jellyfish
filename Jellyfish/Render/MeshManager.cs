44b41ab92ed570b32323b8c87ca91dc0aa2b66c0
StanR <hi@stanr.info>
2026-05-23 04:03:35 +0500

    Clean up mesh manager slightly

--- Jellyfish/Render/MeshManager.cs
+++ Jellyfish/Render/MeshManager.cs
@@ -56,50 +56,16 @@ public class MeshManager
         _drawing = true;
         var drawStopwatch = Stopwatch.StartNew();
 
-        var sortingPosition = frustum?.NearPlaneCenter ?? Engine.MainViewport.Position;
+        DrawOpaque(drawDev, shaderToUse, frustum);
+        DrawTranslucent(drawDev, shaderToUse, frustum);
 
-        var opaqueObjects = _meshes.Where(x => !(x.Material?.GetParam<bool>("AlphaTest") ?? false)).ToArray();
-        var transluscentObjects = _meshes.Where(x => !opaqueObjects.Contains(x))
-            .OrderByDescending(x => ((x.Position + x.BoundingBox.Center) - sortingPosition).Length)
-            .ToArray();
-
-        var stopwatch = Stopwatch.StartNew();
-        foreach (var mesh in opaqueObjects)
-            DrawMesh(mesh, drawDev, shaderToUse, frustum);
-        PerformanceMeasurment.Add("MeshManager.Draw.Opaque", stopwatch.Elapsed.TotalMilliseconds);
-
-        GL.DepthMask(false);
-        GL.Enable(EnableCap.Blend);
-        GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
-        GL.BlendEquation(BlendEquationMode.FuncAdd);
-
-        stopwatch.Restart();
-        foreach (var mesh in transluscentObjects)
-            DrawMesh(mesh, drawDev, shaderToUse, frustum);
-        PerformanceMeasurment.Add("MeshManager.Draw.Transluscent", stopwatch.Elapsed.TotalMilliseconds);
-
-        GL.Disable(EnableCap.Blend);
-        GL.DepthMask(true);
-
-        // ensures that all VBO updates happen post-rendering
-        foreach (var update in _updateQueue)
-        {
-            update.Item1.Update(update.Item2);
-        }
-
-        _updateQueue.Clear();
+        // ensure that all VBO updates happen post-rendering
+        UpdateMeshes();
 
         _drawing = false;
 
-        if (drawDev)
-        {
-            foreach (var singleFrameMesh in _singleFrameMeshes)
-            {
-                RemoveMesh(singleFrameMesh);
-            }
+        PostDraw();
 
-            _singleFrameMeshes.Clear();
-        }
         frustum?.Dispose();
         PerformanceMeasurment.Add("MeshManager.Draw", drawStopwatch.Elapsed.TotalMilliseconds);
     }
@@ -110,43 +76,49 @@ public class MeshManager
         var drawStopwatch = Stopwatch.StartNew();
 
         using var playerFrustum = Engine.MainViewport.GetFrustum();
-        var playerPosition = playerFrustum.NearPlaneCenter;
+
+        DrawOpaque(drawDev, null, playerFrustum, true);
+        DrawTranslucent(drawDev, null, playerFrustum, true);
+
+        PerformanceMeasurment.Add("MeshManager.DrawGBuffer", drawStopwatch.Elapsed.TotalMilliseconds);
+        _drawing = false;
+    }
+
+    private void DrawOpaque(bool drawDev = true, Shader? shaderToUse = null, Frustum? frustum = null, bool gBuffer = false)
+    {
         var opaqueObjects = _meshes.Where(x => !(x.Material?.GetParam<bool>("AlphaTest") ?? false)).ToArray();
-        var transluscentObjects = _meshes.Where(x => !opaqueObjects.Contains(x))
-            .OrderByDescending(x => ((x.Position + x.BoundingBox.Center) - playerPosition).Length)
-            .ToArray();
 
+        var stopwatch = Stopwatch.StartNew();
         foreach (var mesh in opaqueObjects)
-        {
-            if (mesh.IsDev && !drawDev)
-                continue;
+            DrawMesh(mesh, drawDev, shaderToUse, frustum, gBuffer);
 
-            if (mesh.ShouldDraw && playerFrustum.IsInside(mesh.Position + mesh.BoundingBox.Center, mesh.BoundingBox.Length))
-                mesh.DrawGBuffer();
-        }
+        PerformanceMeasurment.Add("MeshManager.Draw.Opaque", stopwatch.Elapsed.TotalMilliseconds);
+    }
+
+    private void DrawTranslucent(bool drawDev = true, Shader? shaderToUse = null, Frustum? frustum = null, bool gBuffer = false)
+    {
+        var sortingPosition = frustum?.NearPlaneCenter ?? Engine.MainViewport.Position;
+
+        var transluscentObjects = _meshes.Where(x => x.Material?.GetParam<bool>("AlphaTest") ?? false)
+            .OrderByDescending(x => ((x.Position + x.BoundingBox.Center) - sortingPosition).Length)
+            .ToArray();
 
         GL.DepthMask(false);
         GL.Enable(EnableCap.Blend);
         GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
         GL.BlendEquation(BlendEquationMode.FuncAdd);
 
+        var stopwatch = Stopwatch.StartNew();
         foreach (var mesh in transluscentObjects)
-        {
-            if (mesh.IsDev && !drawDev)
-                continue;
+            DrawMesh(mesh, drawDev, shaderToUse, frustum, gBuffer);
 
-            if (mesh.ShouldDraw && playerFrustum.IsInside(mesh.Position + mesh.BoundingBox.Center, mesh.BoundingBox.Length))
-                mesh.DrawGBuffer();
-        }
+        PerformanceMeasurment.Add("MeshManager.Draw.Translucent", stopwatch.Elapsed.TotalMilliseconds);
 
         GL.Disable(EnableCap.Blend);
         GL.DepthMask(true);
-
-        PerformanceMeasurment.Add("MeshManager.DrawGBuffer", drawStopwatch.Elapsed.TotalMilliseconds);
-        _drawing = false;
     }
 
-    private void DrawMesh(Mesh mesh, bool drawDev = true, Shader? shaderToUse = null, Frustum? frustum = null)
+    private void DrawMesh(Mesh mesh, bool drawDev = true, Shader? shaderToUse = null, Frustum? frustum = null, bool gBuffer = false)
     {
         if (mesh.IsDev && !drawDev)
             return;
@@ -156,8 +128,32 @@ public class MeshManager
             if (frustum != null && !frustum.Value.IsInside(mesh.Position + mesh.BoundingBox.Center, mesh.BoundingBox.Length))
                 return;
 
-            mesh.Draw(shaderToUse);
+            // todo: this is UGLY and needs to be completely remade
+            if (gBuffer)
+                mesh.DrawGBuffer();
+            else
+                mesh.Draw(shaderToUse);
         }
+    }
+
+    private void UpdateMeshes()
+    {
+        foreach (var update in _updateQueue)
+        {
+            update.Item1.Update(update.Item2);
+        }
+
+        _updateQueue.Clear();
+    }
+
+    private void PostDraw()
+    {
+        foreach (var singleFrameMesh in _singleFrameMeshes)
+        {
+            RemoveMesh(singleFrameMesh);
+        }
+
+        _singleFrameMeshes.Clear();
     }
 
     public void Unload()
