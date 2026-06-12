60bc83b3372f6377ae485fe4089c76cec07584d5
StanR <hi@stanr.info>
2026-06-12 16:31:00 +0500

    Clean up movement code

--- Jellyfish/Entities/Player.cs
+++ Jellyfish/Entities/Player.cs
@@ -1,6 +1,5 @@
 ﻿using Jellyfish.Console;
 using Jellyfish.Input;
-using Jellyfish.Render;
 using Jellyfish.Utils;
 using JoltPhysicsSharp;
 using OpenTK.Mathematics;
@@ -15,22 +14,26 @@ public class Player : BaseEntity, IInputHandler, IHaveFrustum
     public override bool DrawDevCone { get; set; } = true;
 
     private CharacterVirtual? _physCharacter;
+    private bool _crouching;
+    private bool _sprinting;
+    private bool _jumping;
 
-    private const float camera_speed = 120.0f;
+    private const float walk_velocity = 120.0f;
     private const float jump_velocity = 170.0f;
     private const float sensitivity = 0.2f;
 
     private const float height = 65;
     private const float width = 30;
 
-    public override BoundingBox? BoundingBox { get; } =
-        new BoundingBox(new Vector3(width / 2, height / 2, width / 2), new Vector3(-width / 2, -height / 2, -width / 2));
+    private Vector3 _desiredDirection = Vector3.Zero;
+
+    public override BoundingBox? BoundingBox { get; } = new(new Vector3(width / 2, height / 2, width / 2), new Vector3(-width / 2, -height / 2, -width / 2));
 
     public override void Think(float frameTime)
     {
         if (!ConVarStorage.Get<bool>("edt_enable"))
         {
-            Engine.MainViewport.Position = GetPropertyValue<Vector3>("Position") + new Vector3(0, height / 2, 0);
+            Engine.MainViewport.Position = GetPropertyValue<Vector3>("Position") + new Vector3(0, height / (_crouching ? 4 : 2), 0);
             Engine.MainViewport.Rotation = GetPropertyValue<Quaternion>("Rotation");
         }
         else
@@ -76,7 +79,37 @@ public class Player : BaseEntity, IInputHandler, IHaveFrustum
         Engine.MainViewport.Pitch -= mouseState.Delta.Y * sensitivity;
         Engine.InputManager.IsControllingCursor = true;
 
-        return PhysicsMove(keyboardState) || mouseState.Delta != Vector2.Zero;
+        if (_physCharacter == null)
+            return false;
+
+        var direction = Vector3.Zero;
+
+        var forward = Engine.MainViewport.Front;
+        var side = Engine.MainViewport.Right;
+
+        // forward 
+        if (keyboardState.IsKeyDown(Keys.W))
+            direction += new Vector3(forward.X, 0, forward.Z);
+
+        // backwards
+        if (keyboardState.IsKeyDown(Keys.S))
+            direction += -new Vector3(forward.X, 0, forward.Z);
+
+        // left
+        if (keyboardState.IsKeyDown(Keys.A))
+            direction += -new Vector3(side.X, 0, side.Z);
+
+        // right
+        if (keyboardState.IsKeyDown(Keys.D))
+            direction += new Vector3(side.X, 0, side.Z);
+
+        _crouching = keyboardState.IsKeyDown(Keys.LeftControl);
+        _sprinting = keyboardState.IsKeyDown(Keys.LeftShift);
+        _jumping = keyboardState.IsKeyDown(Keys.Space);
+
+        _desiredDirection = direction;
+
+        return _desiredDirection != Vector3.Zero || mouseState.Delta != Vector2.Zero;
     }
 
     private void SimulatePhysics(float frameTime)
@@ -102,7 +135,7 @@ public class Player : BaseEntity, IInputHandler, IHaveFrustum
             if (_physCharacter.GroundState != GroundState.OnGround)
                 groundFriction = 1f;
 
-            // actively try to stop the movement ground movement
+            // actively try to stop the ground movement
             _physCharacter.LinearVelocity -= groundVelocity * groundFriction * frameTime;
 
             var groundDirection = System.Numerics.Vector3.Normalize(groundVelocity);
@@ -111,32 +144,29 @@ public class Player : BaseEntity, IInputHandler, IHaveFrustum
             if (dot <= 0)
                 _physCharacter.LinearVelocity = System.Numerics.Vector3.Zero with { Y = _physCharacter.LinearVelocity.Y };
         }
-    }
 
-    private bool PhysicsMove(KeyboardState keyboardState)
-    {
-        if (_physCharacter == null) 
-            return false;
+        var velocity = walk_velocity;
+        if (_sprinting)
+            velocity *= 2;
+        else if (_crouching)
+            velocity /= 2;
 
-        var cameraSpeed = keyboardState.IsKeyDown(Keys.LeftShift) ? camera_speed * 2 : camera_speed;
-
-        var direction = GetMovementDirection(keyboardState);
+        var direction = _desiredDirection;
 
         var desiredVelocity = Vector3.Zero;
 
         // clamp desired ground velocity
         var directionVelocity = Vector3.Dot((Vector3)_physCharacter.LinearVelocity, direction);
-        if (directionVelocity < cameraSpeed)
+        if (directionVelocity < velocity)
         {
             var airStrafeMultiplier = 1f;
             if (_physCharacter.GroundState == GroundState.InAir)
                 airStrafeMultiplier = 0.05f;
 
-            desiredVelocity += direction * cameraSpeed * airStrafeMultiplier;
+            desiredVelocity += direction * velocity * airStrafeMultiplier;
         }
 
-        // jump
-        if (keyboardState.IsKeyPressed(Keys.Space) && _physCharacter.IsSupported && _physCharacter.GroundState == GroundState.OnGround)
+        if (_jumping && _physCharacter!.IsSupported && _physCharacter!.GroundState == GroundState.OnGround)
         {
             var verticalVelocity = Vector3.Dot((Vector3)_physCharacter.LinearVelocity, Vector3.UnitY);
             if (verticalVelocity < jump_velocity)
@@ -144,40 +174,12 @@ public class Player : BaseEntity, IInputHandler, IHaveFrustum
         }
 
         _physCharacter.LinearVelocity += desiredVelocity.ToNumericsVector();
-
-        return desiredVelocity != Vector3.Zero;
     }
 
     public Frustum GetFrustum()
     {
-        var cameraPosition = GetPropertyValue<Vector3>("Position") + new Vector3(0, height / 2, 0);
+        var cameraPosition = GetPropertyValue<Vector3>("Position") + new Vector3(0, height / (_crouching ? 4 : 2), 0);
         var view = Matrix4.LookAt(cameraPosition, cameraPosition - Vector3.UnitZ, Vector3.UnitY);
         return new Frustum(view * Engine.MainViewport.GetProjectionMatrix());
-    }
-
-    private Vector3 GetMovementDirection(KeyboardState keyboardState)
-    {
-        var direction = Vector3.Zero;
-
-        var forward = Engine.MainViewport.Front;
-        var side = Engine.MainViewport.Right;
-
-        // forward 
-        if (keyboardState.IsKeyDown(Keys.W))
-            direction += new Vector3(forward.X, 0, forward.Z);
-
-        // backwards
-        if (keyboardState.IsKeyDown(Keys.S))
-            direction += -new Vector3(forward.X, 0, forward.Z);
-
-        // left
-        if (keyboardState.IsKeyDown(Keys.A))
-            direction += -new Vector3(side.X, 0, side.Z);
-
-        // right
-        if (keyboardState.IsKeyDown(Keys.D))
-            direction += new Vector3(side.X, 0, side.Z);
-
-        return direction;
     }
 }
